2a742c0dfc19362b6fad18a15f917cb58f65b224
Kianimal <spoono17@gmail.com>
2020-07-10 06:32:56 -0500

    Preparing for Heroku deployment. Reconfigured connection.js to deploy using JawsDB on Heroku.

--- application/config/connection.js
+++ application/config/connection.js
@@ -1,20 +1,19 @@
-// *********************************************************************************
-// CONNECTION.JS - THIS FILE INITIATES THE CONNECTION TO MYSQL
-// *********************************************************************************
-
-// Require mysql
 var mysql = require("mysql");
 
-// Set up our connection information
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "blappers",
-  database: "company_info"
-});
+var connection;
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else{
+  connection = mysql.createConnection({
+    port: 3306,
+    host: "localhost",
+    user: "root",
+    password: "blappers",
+    database: "company_info"
+  });
+}
 
-// Connect to the database
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
@@ -23,5 +22,4 @@ connection.connect(function(err) {
   console.log("connected as id " + connection.threadId);
 });
 
-// Export connection
 module.exports = connection;
